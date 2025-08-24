3a8f053dbb661000dec75c175e9c1f7a0bb3c892
kamaljoshi001 <85295309+kamaljoshi001@users.noreply.github.com>
2025-08-24 22:51:41 +0530

    code updated

--- 01.py
+++ 01.py
@@ -4,3 +4,8 @@ tempreatures = [32.5,31.8,33.0,35.2,36.6]
 arr = np.array(tempreatures)
 average = np.mean(arr)
 print(average)
+
+python_list = [1,2,3,4,5,6]  # python_list → a list object (<class 'list'>)
+print(python_list)
+numpy_arr = np.array([1,2,3,4,5,6]) # numpy_arr → a NumPy ndarray (<class 'numpy.ndarray'>)
+print(numpy_arr)
