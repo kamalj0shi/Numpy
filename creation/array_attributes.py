d67668509bfdcd967a2bf0e6e814ba4f4539c6a1
kamalj0shi <kamaljoshiuk03@gmail.com>
2025-08-26 23:57:03 +0530

    code updated

---
+++ creation/array_attributes.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr_2d = np.array([[1,2,3],
+                  [3,4,5]])
+# print(arr_2d.shape) #(2,3)
+# print(arr_2d.size) #6
+# print(arr_2d.dtype) #int64
+# print(arr_2d.ndim)
+
