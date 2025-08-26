acfc9ec7c32c1ef7d51beb97514cf45df8241146
kamalj0shi <kamaljoshiuk03@gmail.com>
2025-08-27 00:09:06 +0530

    code updated again

--- creation/array_attributes.py
+++ creation/array_attributes.py
@@ -7,3 +7,10 @@ arr_2d = np.array([[1,2,3],
 # print(arr_2d.dtype) #int64
 # print(arr_2d.ndim)
 
+arr = np.array([1.2,2.5,30.5,4.0])
+#arr.astype(datatype) # data type could be int,str,float etc
+
+int_arr = arr.astype(int)
+print(int_arr)
+
+# mathmatical operation on numpy arrays : --
